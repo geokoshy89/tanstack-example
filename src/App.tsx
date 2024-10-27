d434f1eb3f1945ccbbffed03df7cde637d39e498
George <geo.koshy89@gmail.com>
2024-10-27 15:35:52 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery } from '@tanstack/react-query';
+const fetchUser = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
 
 function App() {
+  const [users, setUsers] = useState<any>([]);
+  const { data, error, isLoading } = useQuery(['user'], fetchUser);
+
+  useEffect(() => {
+    setUsers(data)
+  },[data])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {JSON.stringify(users)}
     </div>
   );
 }
